c7c0d766cb8fc928ef07b8af497ece244dc2f55e
PeaceGH <pichayuthkitti@gmail.com>
2015-10-17 01:40:05 +0700

    group detail

--- PeerShare/EventViewController.swift
+++ PeerShare/EventViewController.swift
@@ -19,16 +19,18 @@ class EventViewController: UIViewController {
         var y = CGFloat(10)
         var y_start = CGFloat(119.0)
         var x_start = CGFloat(10.0)
-        var count = 0
+        var count = 1
         
-        for var i = count; i < 8; ++i,++count{
+        for var i = count; i <= 8; ++i,++count{
             
-            var circle = UIView(frame: CGRectMake(x_start, y_start, square_x, square_x))
+            var circleBtn = UIButton(frame: CGRectMake(x_start, y_start, square_x, square_x))
             var label = UILabel(frame: CGRectMake(x_start+18, y_start+110, 80, 21))
-            circle.backgroundColor = UIColor.redColor()
-            circle.layer.cornerRadius = square_x/2.0;
+            circleBtn.backgroundColor = UIColor.redColor()
+            circleBtn.layer.cornerRadius = square_x/2.0;
+            circleBtn.tag = i
+            circleBtn.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
             
-            self.view.addSubview(circle)
+            self.view.addSubview(circleBtn)
             
             x_start = (x_start + y + square_x)
             
@@ -37,18 +39,27 @@ class EventViewController: UIViewController {
             label.text = "I'am a test label"
             self.view.addSubview(label)
             
-            
-            
-            if count == 2 {
-                y_start = y_start+square_x+21+y
+            if count % 3 == 0{
+                y_start = y_start+square_x+21
                 x_start = CGFloat(10.0)
-                count = 0
+                //count = 0
             }
-            
-            
             
         }
 
+    }
+    
+    func buttonClicked(a:UIButton) -> Void
+    {
+        print("Button \(a.tag) was clicked!")
+        
+        var vc = self.storyboard?.instantiateViewControllerWithIdentifier("groupdetailviewcontroller").childViewControllers.first as! GroupDetailViewController
+        vc.groupTitle = "TEST"
+        vc.groupMemberNumber = "4"
+        
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+        
     }
 
     
---
+++ PeerShare/GroupDetailViewController.swift
@@ -0,0 +1,63 @@
+//
+//  GroupDetailViewController.swift
+//  PeerShare
+//
+//  Created by Peace's Toy on 10/16/2558 BE.
+//  Copyright © 2558 Fifa'Operation. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var groupTitle:String = ""
+    var groupMemberNumber:String = ""
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var GroupImg: UIView!
+    @IBOutlet weak var memberNumber: UILabel!
+
+    @IBOutlet weak var kPrice: UILabel!
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = groupTitle
+        GroupImg.layer.cornerRadius = 125/2.0
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        memberNumber.text = groupMemberNumber
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
+        
+        cell.textLabel?.text = "PEACE"
+        cell.imageView?.image = UIImage(named: "testimage")
+        cell.detailTextLabel?.text = "Test"
+       
+        
+    
+        
+        return cell!
+    }
+
+}
